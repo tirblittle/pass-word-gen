09359ffca54e1b7c00591e10221c7c0b63f2eb85
kieu <nu_kieu_giang.hoang@p7s1applications.de>
2019-02-01 15:20:39 +0100

    first commit

---
+++ src/main/java/PasswordGenerator.java
@@ -0,0 +1,13 @@
+public class PasswordGenerator {
+    private Integer passwordNumber;
+    private String passwordName;
+
+    public PasswordGenerator(Integer passwordNumber, String passwordName) {
+        this.passwordNumber = passwordNumber;
+        this.passwordName = passwordName;
+    }
+
+    public void createRandomPassword() {
+
+    }
+}
